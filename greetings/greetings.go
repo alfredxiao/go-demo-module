dd09155df1cdc5e6f72bd058939e48dc8590d507
Alfred Xiao <alfred.xiao@qantashotels.com>
2021-02-11 22:26:36 +1100

    update to v1.0.2

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -9,6 +9,6 @@ import (
 // Hello returns a greeting for the named person.
 func Hello(name string) string {
 	// Return a greeting that embeds the name in a message.
-	message := fmt.Sprintf("v1.0.1 %v", name, quote.Go())
+	message := fmt.Sprintf("v1.0.2 %v", name, quote.Go())
 	return message
 }
