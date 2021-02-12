69ea2bc8b2e25f3dd8ca3d9375e2b55e6f0f48c0
Alfred Xiao <alfred.xiao@qantashotels.com>
2021-02-13 08:55:24 +1100

    up to 222

--- v2/go.mod
+++ v2/go.mod
@@ -1,3 +1,5 @@
 module github.com/alfredxiao/go-demo-module/v2
 
 go 1.15
+
+require rsc.io/quote v1.5.2
--- v2/greetings/greetings.go
+++ v2/greetings/greetings.go
@@ -1,14 +1,18 @@
 package greetings
 
 import (
+	"errors"
 	"fmt"
 
 	"rsc.io/quote"
 )
 
 // Hello returns a greeting for the named person.
-func Hello(name string) string {
+func Hello(name string) (string, error) {
+	if name == "" {
+		return "", errors.New("name is empty")
+	}
 	// Return a greeting that embeds the name in a message.
-	message := fmt.Sprintf("v2.0.2 %v, %v", name, quote.Go())
-	return message
+	message := fmt.Sprintf("v2.2.2 %v, %v", name, quote.Go())
+	return message, nil
 }
